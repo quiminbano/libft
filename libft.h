dce78f3c37df5deaddae86499ea4da1f7fca0763
Carlos Orellana <corellana1988@gmail.com>
2024-05-10 14:05:30 +0300

    undoing the inclusion of stdint.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 08:50:51 by corellan          #+#    #+#             */
-/*   Updated: 2024/05/10 13:52:09 by corellan         ###   ########.fr       */
+/*   Updated: 2024/05/10 14:01:10 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 # include "gnl/get_next_line.h"
 # include "ft_printf/ft_printf.h"
-# ifdef __linux__
-#  include <stdint.h>
-# else
-#  include <limits.h>
-# endif
 
 typedef struct s_list
 {
