4525beba52832b154f2b671bed8bca1826a1986f
Carlos Orellana <corellana1988@gmail.com>
2024-11-13 19:50:39 +0200

    calloc improved

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 16:40:46 by corellan          #+#    #+#             */
-/*   Updated: 2023/11/14 17:44:04 by corellan         ###   ########.fr       */
+/*   Updated: 2024/11/12 20:14:20 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,14 @@
 void	*ft_calloc(size_t nitems, size_t size)
 {
 	void	*s;
-	size_t	alloc_max;
+	size_t	result;
 
-	alloc_max = ((size_t)1 << 31);
-	if ((nitems > 0) && (size > 0))
-	{
-		if ((((nitems * size) % size) != 0) || \
-			(((nitems * size) % nitems) != 0))
-			return (NULL);
-	}
-	if ((nitems * size) > alloc_max)
+	result = size * nitems;
+	if (size > 0 && nitems != result / size)
 		return (NULL);
-	s = (void *)malloc(size * nitems);
+	s = (void *)malloc(result);
 	if (s == NULL)
 		return (NULL);
-	ft_bzero(s, (nitems * size));
+	ft_bzero(s, result);
 	return (s);
 }
